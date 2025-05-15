4e5bdc6161094634a704c7c4830264e3b2025eab
olsadomi <77245008+olsadomi@users.noreply.github.com>
2025-05-15 12:00:38 +0200

    created class KeyStore, created static variables, HashMaps, for saving public and private keys

---
+++ src/main/java/detyra/KeyStore.java
@@ -0,0 +1,12 @@
+package detyra;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeyStore {
+    private static final Map<String, PublicKey> publicKeys = new HashMap<>();
+    private static final Map<String, PrivateKey> privateKeys = new HashMap<>();
+
+}
