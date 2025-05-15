73e67278d5c0aefa6354371c4acfe1efcacdf82b
olsadomi <77245008+olsadomi@users.noreply.github.com>
2025-05-15 12:01:41 +0200

    added getters for public and private keys

--- src/main/java/detyra/KeyStore.java
+++ src/main/java/detyra/KeyStore.java
@@ -9,4 +9,13 @@ public class KeyStore {
     private static final Map<String, PublicKey> publicKeys = new HashMap<>();
     private static final Map<String, PrivateKey> privateKeys = new HashMap<>();
 
+    public static PublicKey getPublicKey(String username) {
+        return publicKeys.get(username);
+    }
+
+    public static PrivateKey getPrivateKey(String username) {
+        return privateKeys.get(username);
+    }
+
+
 }
