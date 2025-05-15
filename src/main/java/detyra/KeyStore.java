6cbd6e6999b2b605463a286bdf38c4a285a06610
olsadomi <77245008+olsadomi@users.noreply.github.com>
2025-05-16 00:32:01 +0200

    modified class KeyStore to save save keys into .ser files, using method savekeys() called in addkeys premade method. loadkeys method uses FileInputStream class to manipulate with outer files and generate/store the keys

--- src/main/java/detyra/KeyStore.java
+++ src/main/java/detyra/KeyStore.java
@@ -1,13 +1,25 @@
 package detyra;
 
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.security.*;
+import java.util.*;
 
 public class KeyStore {
-    private static final Map<String, PublicKey> publicKeys = new HashMap<>();
-    private static final Map<String, PrivateKey> privateKeys = new HashMap<>();
+    private static final String PUBLIC_KEYS_FILE = "public_keys.ser";
+    private static final String PRIVATE_KEYS_FILE = "private_keys.ser";
+
+    private static Map<String, PublicKey> publicKeys = new HashMap<>();
+    private static Map<String, PrivateKey> privateKeys = new HashMap<>();
+
+    static {
+        loadKeys();
+    }
+
+    public static void addKeys(String username, PublicKey publicKey, PrivateKey privateKey) {
+        publicKeys.put(username, publicKey);
+        privateKeys.put(username, privateKey);
+        saveKeys();
+    }
 
     public static PublicKey getPublicKey(String username) {
         return publicKeys.get(username);
@@ -17,16 +29,38 @@ public class KeyStore {
         return privateKeys.get(username);
     }
 
-    public static void addKeys(String username, PublicKey publicKey, PrivateKey privateKey) {
-        publicKeys.put(username, publicKey);
-        privateKeys.put(username, privateKey);
-    }
-
     public static boolean hasKeys(String username) {
         return publicKeys.containsKey(username) && privateKeys.containsKey(username);
     }
 
+    private static void saveKeys() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(PUBLIC_KEYS_FILE))) {
+            oos.writeObject(publicKeys);
+        } catch (IOException e) {
+            System.out.println("Failed to save public keys: " + e.getMessage());
+        }
 
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(PRIVATE_KEYS_FILE))) {
+            oos.writeObject(privateKeys);
+        } catch (IOException e) {
+            System.out.println("Failed to save private keys: " + e.getMessage());
+        }
+    }
 
+    @SuppressWarnings("unchecked")
+    private static void loadKeys() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(PUBLIC_KEYS_FILE))) {
+            publicKeys = (Map<String, PublicKey>) ois.readObject();
+        } catch (FileNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Failed to load public keys: " + e.getMessage());
+        }
 
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(PRIVATE_KEYS_FILE))) {
+            privateKeys = (Map<String, PrivateKey>) ois.readObject();
+        } catch (FileNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Failed to load private keys: " + e.getMessage());
+        }
+    }
 }
