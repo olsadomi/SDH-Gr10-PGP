90af25ea2c66c14c6371f37911de60427890f945
natyrabajgora <natyrabajgora11@gmail.com>
2025-05-15 17:31:00 +0200

    Krijimi i konstruktorit dhe getter per perfaqsimin e email

--- src/main/java/detyra/Email.java
+++ src/main/java/detyra/Email.java
@@ -1,4 +1,20 @@
 package detyra;
 
 public class Email {
+    private String from;
+    private String to;
+    private String content;
+    private String signature;
+
+    public Email(String from, String to, String content, String signature) {
+        this.from = from;
+        this.to = to;
+        this.content = content;
+        this.signature = signature;
+    }
+
+    public String getFrom() { return from; }
+    public String getTo() { return to; }
+    public String getContent() { return content; }
+    public String getSignature() { return signature; }
 }
